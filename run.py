6ddac6bb7cee7b1bc3a87ca943518bac67318111
Ilya Nazarov <ilya95@mail.com>
2020-01-16 18:41:43 +0300

    Made output table and plots, remove unused code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import math
-import pylab
+import matplotlib.pyplot as plt
 
 
 class Text:
@@ -66,26 +66,6 @@ def get_strings(n):
 	return texts
 
 
-def all_list(l1, l2, l3, l4):
-	l = []
-	l.extend(l1)
-	l.extend(l2)
-	l.extend(l3)
-	l.extend(l4)
-	return l
-
-
-def serial_number(all_l, txt_evrchl):
-	clist = []  # list of coinsiding values
-	counter = 0
-	for i in all_l:
-		counter += 1
-		for n in txt_evrchl:
-			if n == i:
-				clist.append(counter)
-	return clist
-
-
 def form_output(rows_list):
 	table_rows = []
 	line2 = f'{"Кол-во":^10}|{"p":^10}|{"H":^10}|'*4
@@ -120,112 +100,18 @@ def main():
 			rows.append(text.get_value(v))
 	letters = texts[0].get_value('letters')
 	rows[0] = letters
-	form_output(rows)
+	table = form_output(rows)
 
-	# all_lst = all_list(text1_list, text2_list, text3_list, text4_list)
-	# sn1 = serial_number(all_l, text1_every_char_list)
-	# sn2 = serial_number(all_l, text2_every_char_list)
-	# sn3 = serial_number(all_l, text3_every_char_list)
-	# sn4 = serial_number(all_l, text4_every_char_list)
-	# graph = []
-	# graph2 = []
-	# graph3 = []
-	# graph4 = []
-	# numg1 = []
-	# numg2 = []
-	# numg3 = []
-	# numg4 = []
-	# pi12 = pi1
-	# pi22 = pi2
-	# pi32 = pi3
-	# pi42 = pi4
-	# # cpi1 =
-	# # cpi2 = chr(pi2)
-	# # cpi3 = chr(pi3)
-	# # cpi4 = chr(pi4)
-	# counter = 0
-	# elist = []
-	# for i in text1_every_char_list:
-	# 	elist.append(i)
-	# for i in text2_every_char_list:
-	# 	if elist.count(i) == 0:
-	# 		elist.insert(counter, i)
-	# for i in text3_every_char_list:
-	# 	if elist.count(i) == 0:
-	# 		elist.append(i)
-	# for i in text4_every_char_list:
-	# 	if elist.count(i) == 0:
-	# 		elist.append(i)
-	# for i in elist:
-	# 	if text1_every_char_list.count(i) == 0:
-	# 		pi12.insert(counter, 0)
-	# 	counter += 1
-	# counter = 0
-	# for i in elist:
-	# 	if text2_every_char_list.count(i) == 0:
-	# 		pi22.insert(counter, 0)
-	# 	counter += 1
-	# counter = 0
-	# for i in elist:
-	# 	if text3_every_char_list.count(i) == 0:
-	# 		pi32.insert(counter, 0)
-	# 	counter += 1
-	# counter = 0
-	# for i in elist:
-	# 	if text4_every_char_list.count(i) == 0:
-	# 		pi42.insert(counter, 0)
-	# 	counter += 1
-	# counter = 0
-	#
-	# for i in elist:
-	# 	graph.append(ord(i))
-	# for i in range(len(pi12)):
-	# 	numg1.append(i)
-	# for i in range(len(pi22)):
-	# 	numg2.append(i)
-	# for i in range(len(pi32)):
-	# 	numg3.append(i)
-	# for i in range(len(pi42)):
-	# 	numg4.append(i)
-	#
-	# # pylab.annotate(
-	# #   'message',
-	# #  xy=(6, 30),
-	# # xytext=(8, 31.5),
-	# # arrowprops={
-	# #   'facecolor': 'black',
-	# #  'shrink': 0.05
-	# # });
-	#
-	# ###pylab.plot(221)
-	# ##pylab.plot (numg1,pi1)
-	# ##pylab.xticks(numg1,graph)
-	# ##pylab.xlabel('Код символа')
-	# ##pylab.ylabel('Вероятность')
-	# ##pylab.title('Английский текст №1',fontsize=10)
-	# ##
-	# ###pylab.subplot(222)
-	# ##pylab.plot (numg3, pi3)
-	# ##pylab.xticks(numg3,graph)
-	# ##pylab.xlabel('Код символа')
-	# ##pylab.ylabel('Вероятность')
-	# ##pylab.title('Русский текст №1',fontsize=10)
-	# ##
-	# ###pylab.subplot(223)
-	# ##pylab.plot (numg2, pi2)
-	# ##pylab.xticks(numg2,graph)
-	# ##pylab.xlabel('Код символа')
-	# ##pylab.ylabel('Вероятность')
-	# ##pylab.title('Английский текст №2',fontsize=10)
-	#
-	# # pylab.subplot(224)
-	# pylab.plot(numg4, pi4)
-	# pylab.xticks(numg4, graph)
-	# pylab.xlabel('Код символа')
-	# pylab.ylabel('Вероятность')
-	# pylab.title('Русский текст №2', fontsize=10)
-	#
-	# pylab.show()
+	print(table)
+	#plt.figure()
+	for i in range(2, 12, 3):
+		plt.figure()
+		if i in (2, 5):
+			x, y = rows[0][:-33], rows[i][:-33]
+		else:
+			x, y = rows[0][26:], rows[i][26:]
+		plt.plot(x, y)
+	plt.show()
 
 
 if __name__ == "__main__":
